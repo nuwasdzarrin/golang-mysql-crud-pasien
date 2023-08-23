fff6ebe248082f8a0488bacb58c52939248c0d71
nuwasdzarrin <nuwasdzarrin3@gmail.com>
2023-08-23 23:13:21 +0700

    complete

--- controllers/pasiencontroller/pasiencontroller.go
+++ controllers/pasiencontroller/pasiencontroller.go
@@ -1,11 +1,13 @@
 package pasiencontroller
 
 import (
+	"fmt"
 	"github.com/jeypc/go-crud/entities"
 	"github.com/jeypc/go-crud/libraries"
 	"github.com/jeypc/go-crud/models"
 	"html/template"
 	"net/http"
+	"strconv"
 )
 
 var validation = libraries.NewValidation()
@@ -58,14 +60,27 @@ func Add(response http.ResponseWriter, request *http.Request) {
 
 func Edit(response http.ResponseWriter, request *http.Request) {
 	if request.Method == http.MethodGet {
+		queryString := request.URL.Query()
+		id, _ := strconv.ParseInt(queryString.Get("id"), 10, 64)
+
+		var pasien entities.Pasien
+		err := pasienModel.Find(id, &pasien)
+		if err != nil {
+			return
+		}
+		data := map[string]interface{}{
+			"pasien": pasien,
+		}
+
 		temp, err := template.ParseFiles("views/pasien/edit.html")
 		if err != nil {
 			panic(err)
 		}
-		temp.Execute(response, nil)
+		temp.Execute(response, data)
 	} else if request.Method == http.MethodPost {
 		request.ParseForm()
 		var pasien entities.Pasien
+		pasien.Id, _ = strconv.ParseInt(request.Form.Get("id"), 10, 64)
 		pasien.NamaLengkap = request.Form.Get("nama_lengkap")
 		pasien.NIK = request.Form.Get("nik")
 		pasien.JenisKelamin = request.Form.Get("jenis_kelamin")
@@ -80,16 +95,23 @@ func Edit(response http.ResponseWriter, request *http.Request) {
 			data["pasien"] = pasien
 			data["validation"] = vError
 		} else {
-			pasienModel.Create(pasien)
-			data["pesan"] = "Data pasien berhasil disimpan"
+			err := pasienModel.Update(pasien)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			data["pesan"] = "Data pasien berhasil diperbarui"
 		}
 
-		temp, _ := template.ParseFiles("views/pasien/add.html")
+		temp, _ := template.ParseFiles("views/pasien/edit.html")
 		temp.Execute(response, data)
 	}
 
 }
 
 func Delete(response http.ResponseWriter, request *http.Request) {
-
+	queryString := request.URL.Query()
+	id, _ := strconv.ParseInt(queryString.Get("id"), 10, 64)
+	pasienModel.Delete(id)
+	http.Redirect(response, request, "/pasien", http.StatusSeeOther)
 }
--- models/pasienmodel.go
+++ models/pasienmodel.go
@@ -59,5 +59,26 @@ func (p *PasienModel) GetAll() ([]entities.Pasien, error) {
 }
 
 func (p *PasienModel) Find(id int64, pasien *entities.Pasien) error {
+	return p.conn.QueryRow("select * from pasien where id = ?", id).Scan(&pasien.Id, &pasien.NamaLengkap,
+		&pasien.NIK, &pasien.JenisKelamin, &pasien.TempatLahir, &pasien.TanggalLahir, &pasien.Alamat, &pasien.NoHP)
 
+}
+
+func (p *PasienModel) Update(pasien entities.Pasien) error {
+	_, err := p.conn.Exec(
+		"update pasien set nama_lengkap = ?, nik = ?, jenis_kelamin = ?, tempat_lahir = ?, tanggal_lahir = ?, alamat = ?, no_hp = ? where id = ?",
+		pasien.NamaLengkap, pasien.NIK, pasien.JenisKelamin, pasien.TempatLahir, pasien.TanggalLahir, pasien.Alamat, pasien.NoHP, pasien.Id,
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *PasienModel) Delete(id int64) {
+	_, err := p.conn.Exec("delete from pasien where id = ?", id)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
